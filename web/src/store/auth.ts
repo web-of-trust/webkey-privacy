cfff4447f374a50b491c746bde9472e9fb97308f
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2023-07-05 16:52:16 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- web/src/store/auth.ts
+++ web/src/store/auth.ts
@@ -1,10 +1,44 @@
 // Utilities
+import axios from 'axios'
 import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    auth_token: null,
-    user_name: '',
-    user_role: 'authencated_user'
+    token: localStorage.getItem('AUTH_TOKEN'),
+    user: {
+      displayName: localStorage.getItem('USER_DISPLAY_NAME'),
+      email: localStorage.getItem('USER_EMAIL'),
+    },
   }),
+
+  getters: {
+    token: (state) => state.token,
+    user: (state) => state.user,
+  },
+
+  actions: {
+    async login(username: string, password: string) {
+      await axios.post('/login', {
+        username: username,
+        password: password,
+      })
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+         console.log(error)
+      });
+    },
+
+    logout() {
+      this.token = null
+      this.user = {
+        displayName: '',
+        email: '',
+      }
+      localStorage.removeItem('AUTH_TOKEN')
+      localStorage.removeItem('USER_DISPLAY_NAME')
+      localStorage.removeItem('USER_EMAIL')
+    },
+  },
 })
