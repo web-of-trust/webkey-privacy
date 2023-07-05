d8379b67364217baafe5d9683917124d600f70ea
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2023-07-05 17:46:17 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- web/src/main.ts
+++ web/src/main.ts
@@ -13,6 +13,10 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.APP_BASE_URL
+
 const app = createApp(App)
 
 registerPlugins(app)
