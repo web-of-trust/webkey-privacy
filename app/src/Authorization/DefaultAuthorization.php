4cb4264c85c033945ca7c2cfaad721b5622dd7f1
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2023-07-11 17:18:24 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- app/src/Authorization/DefaultAuthorization.php
+++ app/src/Authorization/DefaultAuthorization.php
@@ -1,4 +1,11 @@
 <?php declare(strict_types=1);
+/**
+ * This file is part of the Webkey Privacy project.
+ *
+ * Licensed under GNU Affero General Public License v3.0
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace App\Authorization;
 
