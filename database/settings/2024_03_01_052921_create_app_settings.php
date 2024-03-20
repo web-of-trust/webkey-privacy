4434dd562cdea35894f8a9974d9f06f1057f6097
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-03-20 14:20:50 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- database/settings/2024_03_01_052921_create_app_settings.php
+++ database/settings/2024_03_01_052921_create_app_settings.php
@@ -6,7 +6,7 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('app_settings.password_store', '');
+        $this->migrator->add('app_settings.password_store', 'local');
         $this->migrator->add('app_settings.password_length', 32);
         $this->migrator->add('app_settings.key_type', '');
         $this->migrator->add('app_settings.elliptic_curve', '');
