a54ed085a302e68edda46dab8b8b45cd06cc8238
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-03-20 16:32:07 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- app/Filament/Resources/X509CertificateResource.php
+++ app/Filament/Resources/X509CertificateResource.php
@@ -50,7 +50,7 @@ class X509CertificateResource extends Resource
         );
         file_put_contents($filePath, $record->certificate_data);
         return response()->download(
-            $filePath, $record->serial_number . '.pem', [
+            $filePath, $record->serial_number . '.cert', [
                 'Content-Type' => 'application/pkcs8',
             ]
         )->deleteFileAfterSend(true);
