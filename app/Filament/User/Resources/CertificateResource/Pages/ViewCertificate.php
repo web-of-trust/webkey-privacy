255a613d91cf877a85cc2eaf8daeaf615533efe8
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-03-26 08:33:45 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- app/Filament/User/Resources/CertificateResource/Pages/ViewCertificate.php
+++ app/Filament/User/Resources/CertificateResource/Pages/ViewCertificate.php
@@ -8,15 +8,7 @@
 
 namespace App\Filament\User\Resources\CertificateResource\Pages;
 
-use App\Support\Helper;
 use App\Filament\User\Resources\CertificateResource;
-use Filament\Actions\Action;
-use Filament\Infolists\{
-    Components\Fieldset,
-    Components\RepeatableEntry,
-    Components\TextEntry,
-    Infolist,
-};
 use Filament\Resources\Pages\ViewRecord;
 
 /**
@@ -28,68 +20,7 @@ use Filament\Resources\Pages\ViewRecord;
  */
 class ViewCertificate extends ViewRecord
 {
+    use \App\Filament\Concerns\ViewOpenPGPCertificate;
+
     protected static string $resource = CertificateResource::class;
-
-    public function getTitle(): string
-    {
-        return __('View Certificate');
-    }
-
-    public function infolist(Infolist $infolist): Infolist
-    {
-        $this->record->subKeys = Helper::getSubkeys($this->record->key_data);
-        return $infolist->schema([
-            Fieldset::make(__('Certificate Information'))->schema([
-                TextEntry::make('domain.name')->label(__('Domain')),
-                TextEntry::make('primary_user')->label(__('User ID')),
-                TextEntry::make('fingerprint')->formatStateUsing(
-                    fn (string $state) => strtoupper($state)
-                )->label(__('Fingerprint')),
-                TextEntry::make('key_id')->formatStateUsing(
-                    fn (string $state) => strtoupper($state)
-                )->label(__('Key ID')),
-                TextEntry::make('key_algorithm')->formatStateUsing(
-                    fn (int $state) => Helper::keyAlgorithm($state)
-                )->label(__('Key Algorithm')),
-                TextEntry::make('key_strength')
-                    ->suffix(' bits')->label(__('Key Strength')),
-                TextEntry::make('creation_time')->label(__('Creation Time')),
-                TextEntry::make('expiration_time')->label(__('Expiration Time')),
-            ]),
-            RepeatableEntry::make('subKeys')
-                ->schema([
-                    TextEntry::make('fingerprint')->formatStateUsing(
-                        fn (string $state) => strtoupper($state)
-                    )->label(__('Fingerprint')),
-                    TextEntry::make('key_id')->formatStateUsing(
-                        fn (string $state) => strtoupper($state)
-                    )->label(__('Key ID')),
-                    TextEntry::make('key_algorithm')->label(__('Key Algorithm')),
-                    TextEntry::make('key_strength')->suffix(' bits')->label(__('Key Strength')),
-                    TextEntry::make('creation_time')->dateTime('Y-m-d H:i:s')->label(__('Creation Time')),
-                    TextEntry::make('expiration_time')->dateTime('Y-m-d H:i:s')->label(__('Expiration Time')),
-                ])->columns(2)->columnSpan(2)->label(__('Sub Keys')),
-            Fieldset::make(__('Revocation'))->schema([
-                TextEntry::make('revocation.tag')->formatStateUsing(
-                    fn (int $state) => self::getResource()::revocationReason($state)
-                )->label(__('Reason')),
-                TextEntry::make('revocation.reason')->label(__('Description')),
-            ])->hidden(!$this->record->is_revoked),
-        ]);
-    }
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Action::make('export')
-                ->action(fn ($record) => Helper::exportOpenPGPKey(
-                    $record->key_id, $record->key_data
-                ))
-                ->icon('heroicon-m-arrow-down-tray')
-                ->label(__('Export Key')),
-            Action::make('back')->url(
-                url()->previous()
-            )->color('gray')->label(__('Back')),
-        ];
-    }
 }
